9a5a4ffad3cf2c753a30411efd3c6497488ab34d
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2023-01-05 15:04:49 +0000

    basic project set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { LoadingButton } from '@mui/lab';
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div
+        style={{
+          background: '#f7f7f7',
+          width: '500px',
+          height: '500px',
+          borderRadius: '1rem',
+          margin: '20rem',
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+        <Button variant='contained' disableRipple={false} fullWidth color='secondary'>Hello</Button>
+
+        <LoadingButton loading variant="outlined">
+          Submit
+        </LoadingButton>
+      </div>
     </div>
   );
 }
