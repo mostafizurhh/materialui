dca288eab473bde766cd549425ad2a831dd8f0b6
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2023-01-07 21:58:26 +0000

    brandingwrapper modified

--- src/Pages/Home/Branding/Branding.js
+++ src/Pages/Home/Branding/Branding.js
@@ -2,39 +2,40 @@ import { Box, Button, Stack, styled, Typography } from '@mui/material';
 import React from 'react';
 
 const BrandingWrapper = styled(Box)(({ theme }) => ({
-    height: '70vh',
+    // height: '70vh',
     backgroundColor: 'rgba(122,178,89,0.15)',
     padding: theme.spacing(4),
     marginTop: theme.spacing(4),
-    clipPath: 'polygon(0% 0%, 100% 0%, 100% 82%, 0% 100%)',
     display: 'flex',
     justifyContent: 'space-between',
+    // alignItems: 'center',
+    gap: 40,
+
 }))
 
 const Branding = () => {
     return (
-        <BrandingWrapper>
+        <BrandingWrapper sx={{ flexDirection: ['column-reverse', 'row'], clipPath: ['inherit', 'polygon(0% 0%, 100% 0%, 100% 82%, 0% 100%)'], height: ['100%', '50vh', '60vh', '70vh'] }}>
             <Stack spacing={5}>
                 <Typography sx={{
                     fontWeight: 'bold',
                     fontSize: '2rem',
-                    lineHeight: '1.2'
+                    lineHeight: '1.2',
                 }}>
                     Let’s Grow <br /> Your Brand <br /> To The Next Level
                 </Typography>
 
                 <Typography sx={{
                     textAlign: 'justify',
-                    width: '90%'
+
                 }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores modi accusantium voluptas? Dolore nisi quis dolorum sit fuga nesciunt, aliquam cum neque quaerat tempore error suscipit, eum at sint excepturi.
                 </Typography>
                 <Button sx={{ width: 150 }}>Hire Us</Button>
             </Stack>
             <Box sx={{
-                width: '90%',
-                display: ['none', 'none', 'block']
+                // display: ['none', 'block']
             }}>
-                <img src='https://i.ibb.co/sPXVMbC/Frame.png' alt="" style={{ width: '100%' }} />
+                <img src='https://i.ibb.co/sPXVMbC/Frame.png' alt="" style={{ width: '100%', height: 'auto' }} />
             </Box>
         </BrandingWrapper>
     );
